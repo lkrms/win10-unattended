6511585ee9da5c69cb908e10df005e44ea958ced
Luke Arms <luke@linacreative.com>
2020-07-28 20:09:05 +1000

    Work around Powershell's stdin buffering, improve connection check

--- specialize.cmd
+++ specialize.cmd
@@ -7,22 +7,22 @@ SET CHOCO_ERRORS=0
 
 IF EXIST "%SCRIPT_DIR%InstallEmbeddedProductKey.ps1" (
 
-    >&2 ECHO Installing embedded product key for Windows
+    ECHO Installing embedded product key for Windows
     powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%InstallEmbeddedProductKey.ps1" || (
         CALL :error "%SCRIPT_DIR%InstallEmbeddedProductKey.ps1" failed
     )
-    >&2 ECHO:
+    ECHO:
 
 )
 
 IF EXIST "%SCRIPT_DIR%Wi-Fi.xml" (
 
     netsh wlan show interfaces >nul || (
-        >&2 ECHO Skipping Wi-Fi setup ^(no WLAN interfaces^)
+        ECHO Skipping Wi-Fi setup ^(no WLAN interfaces^)
         GOTO :checkOnline
     )
 
-    >&2 ECHO Adding Wi-Fi profile from %SCRIPT_DIR%Wi-Fi.xml
+    ECHO Adding Wi-Fi profile from %SCRIPT_DIR%Wi-Fi.xml
     netsh wlan add profile filename="%SCRIPT_DIR%Wi-Fi.xml" || (
         CALL :error "netsh wlan add profile" failed
     )
@@ -33,7 +33,7 @@ IF EXIST "%SCRIPT_DIR%Wi-Fi.xml" (
 CALL :now
 SET START=%NOW%
 SET SECONDS=
->&2 ECHO Waiting for Internet connection
+ECHO Waiting for Internet connection
 
 :checkOnlineAgain
 CALL :online && GOTO :connected
@@ -43,9 +43,11 @@ CALL :now
 SET /A "SECONDS=NOW-START"
 :: Give up after 5 minutes
 IF %SECONDS% GEQ 300 (
-    >&2 ECHO:
-    >&2 ECHO Exiting ^(no Internet connection after 5 minutes^)
-    EXIT /B 1
+    ECHO:
+    ECHO No Internet connection
+    ECHO Will try again after rebooting
+    ping -n 6 127.0.0.1 >nul
+    EXIT /B 2
 )
 :: Sleep for 2 seconds
 ping -n 3 127.0.0.1 >nul
@@ -53,16 +55,16 @@ GOTO :checkOnlineAgain
 
 :connected
 IF DEFINED SECONDS >&2 ECHO:
->&2 ECHO Connection established
->&2 ECHO:
+ECHO Connection established
+ECHO:
 
->&2 ECHO Installing Chocolatey
+ECHO Installing Chocolatey
 powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" || (
-    >&2 ECHO Exiting ^(Chocolatey installation failed^)
-    EXIT /B 1
+    ECHO Exiting ^(Chocolatey installation failed^)
+    EXIT /B 3
 )
 SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
->&2 ECHO:
+ECHO:
 
 :: choco feature enable -n=logEnvironmentValues -y
 choco feature enable -n=useRememberedArgumentsForUpgrades -y
@@ -81,32 +83,32 @@ CALL :choco sumatrapdf --ia="/d ""%ProgramFiles%\SumatraPDF"""
 CALL :choco tightvnc --ia="ADDLOCAL=Server SET_ACCEPTHTTPCONNECTIONS=1 SET_CONTROLPASSWORD=1 SET_PASSWORD=1 SET_RUNCONTROLINTERFACE=1 SET_USECONTROLAUTHENTICATION=1 SET_USEVNCAUTHENTICATION=1 VALUE_OF_ACCEPTHTTPCONNECTIONS=0 VALUE_OF_CONTROLPASSWORD=nZ4yUJ3O VALUE_OF_PASSWORD=Shabbyr= VALUE_OF_RUNCONTROLINTERFACE=0 VALUE_OF_USECONTROLAUTHENTICATION=1 VALUE_OF_USEVNCAUTHENTICATION=1"
 CALL :choco vlc
 
->&2 ECHO %CHOCO_COUNT% packages installed by Chocolatey ^(errors: %CHOCO_ERRORS%^)
->&2 ECHO:
+ECHO %CHOCO_COUNT% packages installed by Chocolatey ^(errors: %CHOCO_ERRORS%^)
+ECHO:
 
 IF NOT EXIST "%SCRIPT_DIR%Office365\setup.exe" GOTO :skipOffice
 IF EXIST "%SCRIPT_DIR%Office365\sources" XCOPY "%SCRIPT_DIR%Office365\sources" C:\OfficeDeploymentTool\sources /E /I /Q /Y
->&2 ECHO Downloading Office 365
+ECHO Downloading Office 365
 "%SCRIPT_DIR%Office365\setup.exe" /download "%SCRIPT_DIR%Office365\Configuration.xml" || (
     CALL :error "%SCRIPT_DIR%Office365\setup.exe" /download "%SCRIPT_DIR%Office365\Configuration.xml" failed
     GOTO :skipOffice
 )
->&2 ECHO Installing Office 365
+ECHO Installing Office 365
 "%SCRIPT_DIR%Office365\setup.exe" /configure "%SCRIPT_DIR%Office365\Configuration.xml" || (
     CALL :error "%SCRIPT_DIR%Office365\setup.exe" /configure "%SCRIPT_DIR%Office365\Configuration.xml" failed
 )
 
 :skipOffice
 IF EXIST "%SCRIPT_DIR%AppAssociations.xml" (
-    >&2 ECHO Configuring default apps
+    ECHO Configuring default apps
     DISM /Online /Import-DefaultAppAssociations:"%SCRIPT_DIR%AppAssociations.xml" || (
         CALL :error "DISM /Online /Import-DefaultAppAssociations" failed
     )
 )
 
->&2 ECHO Exiting ^(end of script^)
->&2 ECHO Errors: %ERRORS%
-GOTO :EOF
+ECHO Exiting ^(end of script^)
+ECHO Errors: %ERRORS%
+EXIT /B 0
 
 
 :now
@@ -120,7 +122,7 @@ ping -4 -n 1 -w 1000 1.1.1.1 | FINDSTR /R /C:"TTL=[0-9][0-9]*$" >nul
 EXIT /B
 
 :choco
->&2 ECHO Installing %1
+ECHO Installing %1
 SET /A "CHOCO_COUNT+=1"
 choco install %* -y --no-progress || (
     CALL :error "choco install %* -y --no-progress" failed
@@ -129,6 +131,6 @@ choco install %* -y --no-progress || (
 EXIT /B
 
 :error
->&2 ECHO ERROR ^(%ERRORLEVEL%^): %*
+ECHO ERROR ^(%ERRORLEVEL%^): %*
 SET /A "ERRORS+=1"
 EXIT /B
