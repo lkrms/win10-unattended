d021ca57e0b5f62d806b667ea3042fdcac5c76c4
Luke Arms <luke@linacreative.com>
2025-06-22 19:39:33 +1000

    Apply settings for test VMs

--- Unattended/Optional/MapNetworkDrives.cmd
+++ Unattended/Optional/MapNetworkDrives.cmd
@@ -7,14 +7,12 @@ SET ERRORS=0
 (
     NET USE /PERSISTENT:NO
 
-    rem Remove persistent connections from H: and S:
+    rem Remove persistent connection from H:
     NET USE H: /DELETE
-    NET USE S: /DELETE
 ) >NUL 2>NUL
 
 @ECHO ON
-NET USE H: \\doo\%USERNAME% || @SET /A "ERRORS+=1"
-NET USE S: \\doo\shared || @SET /A "ERRORS+=1"
+NET USE H: \\192.168.122.1\%USERNAME% || @SET /A "ERRORS+=1"
 @ECHO OFF
 
 IF %ERRORS% EQU 0 EXIT /B 0
