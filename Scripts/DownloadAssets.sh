ca292c96c5bac950af646ee67c20bb8a2ab52e37
Luke Arms <luke@linacreative.com>
2025-07-31 21:09:14 +1000

    Download tools and cache archives in `DownloadAssets.sh`

--- Scripts/DownloadAssets.sh
+++ Scripts/DownloadAssets.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+rufus=4.9
+
 # die [<message>]
 function die() {
     local s=$?
@@ -24,6 +26,8 @@ function download() {
 [[ ${BASH_SOURCE[0]} -ef Scripts/DownloadAssets.sh ]] ||
     die "must run from root of package folder"
 
+mkdir -p Cache
+
 download "https://community.chocolatey.org/install.ps1" Unattended/install.ps1
 
 if [[ -d Office365 ]]; then
@@ -32,9 +36,30 @@ if [[ -d Office365 ]]; then
     download "https://go.microsoft.com/fwlink/?linkid=2196106" Office365/MSTeams-x64.msix
 
     odt_url=$(curl -fsSL "https://www.microsoft.com/en-au/download/details.aspx?id=49117" |
-        grep -Eo "https://download\.microsoft\.com/download/[0-9a-f-]+/officedeploymenttool_[0-9-]+\.exe" |
+        grep -Eo "\<https://download\.microsoft\.com/download/[0-9a-f/-]+/officedeploymenttool_[0-9-]+\.exe\>" |
         head -n1) || die "unable to get Office Deployment Tool URL"
-    echo "==> Downloading Office Deployment Tool from: $odt_url"
-    curl -fL "$odt_url" | bsdtar -xf - -C Office365 EULA setup.exe ||
+    odt_file=Cache/${odt_url##*/}
+    download "$odt_url" "$odt_file"
+    bsdtar -xf "$odt_file" -C Office365 EULA setup.exe ||
         die "error extracting Office Deployment Tool"
+
+    download "https://github.com/Fleex255/PolicyPlus/releases/latest/download/PolicyPlus.exe" Tools/PolicyPlus.exe
+    download "https://github.com/pbatard/rufus/releases/download/v${rufus}/rufus-${rufus}.exe" Tools/rufus.exe
+    download "https://github.com/pbatard/rufus/releases/download/v${rufus}/rufus-${rufus}_x86.exe" Tools/rufus_x86.exe
+    download "https://github.com/pbatard/rufus/releases/download/v${rufus}/rufus-${rufus}_arm64.exe" Tools/rufus_arm64.exe
+
+    lgpo_url=$(curl -fsSL "https://www.microsoft.com/en-au/download/details.aspx?id=55319" |
+        grep -Eo "\<https://download\.microsoft\.com/download/[0-9a-f/-]+/LGPO.zip\>" |
+        head -n1) || die "unable to get LGPO URL"
+    download "$lgpo_url" Cache/LGPO.zip
+    bsdtar -xf Cache/LGPO.zip -C Tools --strip-components 1 "*/LGPO.exe" ||
+        die "error extracting LGPO"
+
+    download "https://download.sysinternals.com/files/ProcessExplorer.zip" Cache/ProcessExplorer.zip
+    bsdtar -xf Cache/ProcessExplorer.zip -C Tools procexp.exe ||
+        die "error extracting Process Explorer"
+
+    download "https://download.sysinternals.com/files/ProcessMonitor.zip" Cache/ProcessMonitor.zip
+    bsdtar -xf Cache/ProcessMonitor.zip -C Tools Procmon.exe ||
+        die "error extracting Process Monitor"
 fi
