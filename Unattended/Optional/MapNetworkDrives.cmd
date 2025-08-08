ec69890f4814afd08d007c98ca537c850ee5c8d5
Luke Arms <luke@linacreative.com>
2025-08-08 15:18:56 +1000

    Detect host IP in `MapNetworkDrives.cmd`

--- Unattended/Optional/MapNetworkDrives.cmd
+++ Unattended/Optional/MapNetworkDrives.cmd
@@ -4,17 +4,28 @@ SETLOCAL
 
 SET ERRORS=0
 
+SET PREFIX=
+FOR /F "delims=" %%G IN (
+    'powershell -NoProfile -Command "(Get-NetIPAddress -AddressFamily IPv4 -AddressState Preferred -PrefixOrigin Dhcp -SkipAsSource $false -Type Unicast).IPAddress -replace \"[^^^^.]+$\""'
+) DO SET PREFIX=%%G
+IF [%PREFIX%]==[] EXIT /B 0
+IF [%PREFIX%]==[10.0.2.] (
+    rem QEMU
+    SET SERVER=10.0.2.2
+) ELSE (
+    rem libvirt/VMware
+    SET SERVER=%PREFIX%1
+)
+
 (
     NET USE /PERSISTENT:NO
 
-    rem Remove persistent connections from H: and S:
+    rem Remove persistent connection from H:
     NET USE H: /DELETE
-    NET USE S: /DELETE
 ) >NUL 2>NUL
 
 @ECHO ON
-NET USE H: \\doo\%USERNAME% || @SET /A "ERRORS+=1"
-NET USE S: \\doo\shared || @SET /A "ERRORS+=1"
+NET USE H: \\%SERVER%\%USERNAME% || @SET /A "ERRORS+=1"
 @ECHO OFF
 
 IF %ERRORS% EQU 0 EXIT /B 0
