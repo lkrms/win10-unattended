e68460fb27b1e7a186bae575e97a65431d6afb74
Luke Arms <luke@linacreative.com>
2025-10-30 09:45:20 +1100

    Make up to 31 attempts to install a winget package before failing
    i.e. try again every 2 minutes for just over an hour; only applies to
    `DOWNLOAD_FAILED` and `INSTALLER_HASH_MISMATCH`, the latter of which is
    usually caused by packages updated hourly

--- Unattended/Optional/InstallCustomApps.cmd
+++ Unattended/Optional/InstallCustomApps.cmd
@@ -28,6 +28,7 @@ SET PKG_COUNT=0
 SET PKG_ERRORS=0
 
 SET "INNO_DEFAULT=/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART"
+SET MAX_ATTEMPTS=31
 
 SET PKG_CRITICAL=
 
@@ -97,7 +98,27 @@ EXIT /B %RESULT%
 CALL :log Deploying %1
 SET /A "PKG_COUNT+=1"
 SET "LOG_FILE=%SystemDrive%\Unattended\Logs\%~n0WinGet-%~nx1.log"
+SET ATTEMPT=1
+:wingetRetry
+SET RETRY=0
 winget install --id %* --scope machine --exact --silent --log "%LOG_FILE%" --accept-source-agreements --disable-interactivity && EXIT /B
+:: DOWNLOAD_FAILED
+IF %ERRORLEVEL% EQU -1978335224 SET RETRY=%ERRORLEVEL%
+:: INSTALLER_HASH_MISMATCH
+IF %ERRORLEVEL% EQU -1978335215 SET RETRY=%ERRORLEVEL%
+:: ERROR_INTERNET_*, ERROR_WINHTTP_* (0x80072ee0 - 0x80072fa1)
+IF %ERRORLEVEL% GEQ -2147012896 IF %ERRORLEVEL% LEQ -2147012703 SET RETRY=%ERRORLEVEL%
+IF %RETRY% EQU 0 GOTO :wingetNoRetry
+IF %ATTEMPT% GEQ %MAX_ATTEMPTS% (
+    CALL :log Upstream error not resolved after %ATTEMPT% attempts, giving up: %1
+    CALL :setErrorLevel %RETRY%
+    GOTO :wingetError
+)
+CALL :log Upstream error on attempt %ATTEMPT% of %MAX_ATTEMPTS%, retrying in 2 minutes: %1
+ping -n 121 127.0.0.1 >NUL
+SET /A "ATTEMPT+=1"
+GOTO :wingetRetry
+:wingetNoRetry
 :: UPDATE_NOT_APPLICABLE
 IF %ERRORLEVEL% EQU -1978335189 EXIT /B 0
 :: Ignore non-critical packages on unsupported hardware
@@ -105,6 +126,7 @@ IF NOT DEFINED PKG_CRITICAL (
     rem NO_APPLICABLE_INSTALLER
     IF %ERRORLEVEL% EQU -1978335216 (CALL :log WARNING ^(%ERRORLEVEL%^): "winget install %1" failed, see %LOG_FILE% & SET /A "PKG_ERRORS+=1" & EXIT /B 0)
 )
+:wingetError
 CALL :error "winget install --id %* --scope machine --exact --silent --accept-source-agreements --disable-interactivity" failed, see %LOG_FILE%
 SET /A "PKG_ERRORS+=1"
 EXIT /B %RESULT%
@@ -122,3 +144,6 @@ SET RESULT=%ERRORLEVEL%
 CALL :log ERROR ^(%RESULT%^): %*
 SET /A "ERRORS+=1"
 EXIT /B %RESULT%
+
+:setErrorLevel
+EXIT /B %~1
